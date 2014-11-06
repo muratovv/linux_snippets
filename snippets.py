121bfdff9e5d0010185e3055a33d84442ab6a03a
Dmitry Tishchenko <flir3d@gmail.com>
2014-11-06 13:00:29 +0300

    Скриптик, работающий с буфером

--- snippets.py
+++ snippets.py
@@ -3,17 +3,21 @@ __author__ = 'flire'
 
 from Xlib import X, XK, display, protocol, ext
 from gi.repository import Gtk
+import pyperclip
 import time
 if __name__ == "__main__":
+    cbcache = pyperclip.paste()
     displ = display.Display()
     focus_request = displ.get_input_focus()
     focus = focus_request.focus
     revert_to = focus_request.revert_to
 
+    pyperclip.copy("Some random text to paste!")
+
     root = Gtk.Window()
 
     def callback(widget, event):
-        keysym = XK.string_to_keysym('A')
+        keysym = XK.string_to_keysym('V')
         keycode = displ.keysym_to_keycode(keysym)
         # ext.xtest.fake_input(displ, X.KeyPress, keycode)
         # ext.xtest.fake_input (displ, X.KeyRelease, keycode)
@@ -29,12 +33,12 @@ if __name__ == "__main__":
             state = X.ControlMask,
             detail = keycode
         )
-        focus.send_event(ev)
+        displ.send_event(focus,ev)
         displ.sync()
+        time.sleep(0.5)
+        pyperclip.copy(cbcache)
         Gtk.main_quit(widget, event)
 
     root.connect("delete-event", callback)
     root.show_all()
     Gtk.main()
-
-
