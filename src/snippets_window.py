e8e41f5a150620dbd5599923bebcf4e475461c04
Semyon Proshev <s-proshev@ya.ru>
2014-11-06 18:22:50 +0300

    Completion

--- src/snippets_window.py
+++ src/snippets_window.py
@@ -2,6 +2,7 @@
 
 from gi.repository import Gtk
 from gi.repository import Gdk
+
 from src.compliter import AutoSub
 
 
@@ -11,38 +12,28 @@ class SnippetsWindow(Gtk.Window):
 
         self.text = ""
 
-        self.box = Gtk.VBox(spacing=5)
-        self.add(self.box)
+        self.completion = Gtk.EntryCompletion()
+        self.completion.set_model(None)
+        self.completion.set_text_column(0)
 
         self.entry = Gtk.Entry()
+        self.entry.set_completion(self.completion)
+
         self.entry.connect("changed", self.on_text_changed)
-        self.entry.connect("key-press-event", self.on_tab_pressed)
-        self.entry.connect("key-release-event", self.on_tab_released)
+        # self.entry.connect("key-press-event", self.on_tab_pressed)
+        # self.entry.connect("key-release-event", self.on_tab_released)
 
-        model = Gtk.ListStore(str)
+        self.add(self.entry)
 
-        self.combo_box = Gtk.ComboBox.new_with_model_and_entry(model)
-        self.combo_box.set_entry_text_column(0)
-        self.combo_box.connect("changed", self.on_combobox_changed)
-
-        self.box.pack_start(self.entry, True, True, 0)
-        self.box.pack_start(self.combo_box, True, True, 0)
-
-        self.asb = AutoSub("snippets")
+        self.auto_sub = AutoSub("src/snippets")
 
     def on_text_changed(self, entry):
-        # TODO send request, update model
-        text = entry.get_text()
+        suggests = Gtk.ListStore(str)
 
-        model = self.combo_box.get_model()
+        for suggest in self.auto_sub.fieldCange_evnt(entry.get_text()):
+            suggests.append([(suggest['label'])])
 
-        model.clear()
-
-        for s in self.asb.fieldCange_evnt(text):
-            model.append([s['label']])
-
-        self.combo_box.set_model(model)
-        self.combo_box.show()
+        self.completion.set_model(suggests)
 
     def on_tab_pressed(self, entry, event, *args):
         if event.keyval == Gdk.KEY_Tab:
@@ -54,22 +45,8 @@ class SnippetsWindow(Gtk.Window):
             # print("TAB_R")
             return True
 
-    def on_combobox_changed(self, combobox):
-        active = combobox.get_active()
 
-        if active != -1:
-            # TODO send result
-            self.text = self.asb.substitution_evnt(combobox.get_model()[active][0])
-
-
-
-# def run(callback):
-#     win = SnippetsWindow()
-#     win.connect("delete-event", Gtk.main_quit)
-#     win.connect("delete-event", callback)
-#     win.show_all()
-#     Gtk.main()
-#     return win
-#
-# if __name__ == '__main__':
-#     run()
+# window = SnippetsWindow()
+# window.connect("delete-event", Gtk.main_quit)
+# window.show_all()
+# Gtk.main()
