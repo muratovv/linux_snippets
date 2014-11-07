9816089a095de987db6637622780526a772e7ead
Semyon Proshev <s-proshev@ya.ru>
2014-11-07 12:20:09 +0300

    Selection and tabs

--- src/snippets_window.py
+++ src/snippets_window.py
@@ -42,15 +42,21 @@ class SnippetsWindow(Gtk.Window):
         entry.set_width_chars(75)
 
         entry.connect("changed", on_text_changed)
-        # entry.connect("key-press-event", self.on_tab_pressed)
-        # entry.connect("key-release-event", self.on_tab_released)
+        entry.connect("key-press-event", self.on_tab_pressed)
+        entry.connect("key-release-event", self.on_tab_released)
 
         return entry
 
     def on_completion_activated(self, entry_completion, model, pos):
         selected = model[pos][0]
+        result = self.auto_sub.substitution_evnt(selected)
 
-        self.entry.set_text(self.auto_sub.substitution_evnt(selected))
+        self.entry.set_text(result)
+
+        l = result.find('#')
+
+        if l != -1:
+            self.entry.select_region(l, result.find('#', l + 1) + 1)
 
         return True
 
@@ -72,16 +78,21 @@ class SnippetsWindow(Gtk.Window):
 
     def on_tab_pressed(self, entry, event, *args):
         if event.keyval == Gdk.KEY_Tab:
-            # print("TAB_P")
             return True
 
     def on_tab_released(self, entry, event, *args):
         if event.keyval == Gdk.KEY_Tab:
-            # print("TAB_R")
+            text = entry.get_text()
+
+            l = text.find('#', entry.get_position())
+
+            if l != -1:
+                entry.select_region(l, text.find('#', l + 1) + 1)
+
             return True
 
-
-# window = SnippetsWindow()
-# window.connect("delete-event", Gtk.main_quit)
-# window.show_all()
-# Gtk.main()
+if __name__ == '__main__':
+    window = SnippetsWindow()
+    window.connect("delete-event", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
